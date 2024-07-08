3e0eaa2357eeb6aad10d5cbb0ef8165ee8085957
Nemanja <nemanja.vasic@productdock.com>
2024-07-08 21:25:24 +0200

    Add script for getting similarity with ollama

--- scripts/similarity-search.ts
+++ scripts/similarity-search.ts
@@ -1,8 +1,12 @@
 import { openaiClient } from "@/utils/openaiClient";
 import { supabaseClient } from "@/utils/supabaseClient";
 import { AIModels } from "@/types";
+import ollama from "ollama";
+import { cosineDistance, desc, gt, sql } from "drizzle-orm";
+import { documents } from "../drizzle-schema";
+import { client, drizzleClient } from "@/utils/pg-drizzle-client";
 
-const executeQuery = async (): Promise<void> => {
+const executeQueryWithOpenAI = async (): Promise<void> => {
   const embeddingResponse = await openaiClient.createEmbedding({
     model: AIModels.OPEN_AI_EMBEDDING,
     input: "In which cities Productdock has offices?",
@@ -24,6 +28,34 @@ const executeQuery = async (): Promise<void> => {
   console.log("DATA", chunks);
 };
 
+const executeQueryWithOllama = async (): Promise<void> => {
+  const embeddingResponse = await ollama.embeddings({
+    model: AIModels.OLLAMA_EMBEDDING,
+    prompt: "Does ProductDock have offices in Banja Luka?",
+  });
+
+  const queryEmbedding = embeddingResponse.embedding;
+
+  const similarity = sql<number>`1 - (
+  ${cosineDistance(documents.embedding, queryEmbedding)}
+  )`;
+
+  const similarDocuments = await drizzleClient
+    .select({
+      title: documents.title,
+      content: documents.content,
+      docsurl: documents.docsurl,
+      similarity,
+    })
+    .from(documents)
+    .where(gt(similarity, 0.5))
+    .orderBy((t) => desc(t.similarity))
+    .limit(1);
+
+  console.log("SIMILAR DOCS", similarDocuments);
+  await client.end();
+};
+
 (async () => {
-  await executeQuery();
+  await executeQueryWithOllama();
 })();
--- src/utils/pg-drizzle-client.ts
+++ src/utils/pg-drizzle-client.ts
@@ -10,7 +10,7 @@ const DB_HOST = process.env.POSTGRES_HOST;
 const DB_PORT = process.env.POSTGRES_PORT;
 const DB = process.env.POSTGRES_DB;
 
-const client = postgres(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB}`, {
+export const client = postgres(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB}`, {
   max: 1,
 });
 export const drizzleClient = drizzle(client);
