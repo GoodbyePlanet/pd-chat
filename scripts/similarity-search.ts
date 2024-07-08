202213e712bfa26f06024d4148cdaa76cc98cc4e
Nemanja <nemanja.vasic@productdock.com>
2024-07-08 21:48:45 +0200

    Add script for chatting with ollama3

--- scripts/similarity-search.ts
+++ scripts/similarity-search.ts
@@ -1,12 +1,13 @@
-import { openaiClient } from "@/utils/openaiClient";
-import { supabaseClient } from "@/utils/supabaseClient";
-import { AIModels } from "@/types";
 import ollama from "ollama";
 import { cosineDistance, desc, gt, sql } from "drizzle-orm";
-import { documents } from "../drizzle-schema";
+import { AIModels } from "@/types";
 import { client, drizzleClient } from "@/utils/pg-drizzle-client";
+import { documents } from "../drizzle-schema";
+import { openaiClient } from "@/utils/openaiClient";
+import { supabaseClient } from "@/utils/supabaseClient";
+import dedent from "ts-dedent";
 
-const executeQueryWithOpenAI = async (): Promise<void> => {
+const similaritySearchWithOpenAI = async (): Promise<void> => {
   const embeddingResponse = await openaiClient.createEmbedding({
     model: AIModels.OPEN_AI_EMBEDDING,
     input: "In which cities Productdock has offices?",
@@ -28,19 +29,28 @@ const executeQueryWithOpenAI = async (): Promise<void> => {
   console.log("DATA", chunks);
 };
 
-const executeQueryWithOllama = async (): Promise<void> => {
-  const embeddingResponse = await ollama.embeddings({
-    model: AIModels.OLLAMA_EMBEDDING,
-    prompt: "Does ProductDock have offices in Banja Luka?",
-  });
+const similaritySearchWithOllama = async (): Promise<void> => {
+  const queryEmbedding = await getOllamaEmbeddings("Does ProductDock have offices in Banja Luka?");
+  const similarDocuments = await getSimilarDocuments(queryEmbedding);
 
-  const queryEmbedding = embeddingResponse.embedding;
+  await client.end();
+};
 
+const getSimilarDocuments = async (
+  queryEmbedding: number[]
+): Promise<
+  {
+    title: string;
+    content: string;
+    docsurl: string;
+    similarity: number;
+  }[]
+> => {
   const similarity = sql<number>`1 - (
   ${cosineDistance(documents.embedding, queryEmbedding)}
   )`;
 
-  const similarDocuments = await drizzleClient
+  return drizzleClient
     .select({
       title: documents.title,
       content: documents.content,
@@ -51,11 +61,54 @@ const executeQueryWithOllama = async (): Promise<void> => {
     .where(gt(similarity, 0.5))
     .orderBy((t) => desc(t.similarity))
     .limit(1);
+};
 
-  console.log("SIMILAR DOCS", similarDocuments);
-  await client.end();
+const getOllamaEmbeddings = async (prompt: string): Promise<number[]> => {
+  const embeddingResponse = await ollama.embeddings({
+    model: AIModels.OLLAMA_EMBEDDING,
+    prompt,
+  });
+
+  return embeddingResponse.embedding;
+};
+
+const createSystemContext = (contentText: string, docsUrl: string): string => {
+  return dedent`You are very enthusiastic representative of Productdock company who loves to help employees. Given the following:
+
+      Context section:
+      ${contentText}
+
+      Answer the questions as truthfully as possible, and if you're unsure of the answer, say 'Sorry, I don't know the answer at this moment. 
+      Please refer to the official documentation ${docsUrl} or ask directly your Unit lead manager.
+    `;
+};
+
+const chatWithOllama3 = async (userInput: string): Promise<void> => {
+  const queryEmbeddings = await getOllamaEmbeddings(userInput);
+  const similarDocuments = await getSimilarDocuments(queryEmbeddings);
+
+  const context = createSystemContext(similarDocuments[0].content, similarDocuments[0].docsurl);
+
+  const response = await ollama.chat({
+    model: AIModels.LLAMA_3,
+    messages: [
+      {
+        role: "system",
+        content: context,
+      },
+      {
+        role: "user",
+        content: userInput,
+      },
+    ],
+    stream: true,
+  });
+
+  for await (const part of response) {
+    process.stdout.write(part.message.content);
+  }
 };
 
 (async () => {
-  await executeQueryWithOllama();
+  await chatWithOllama3("What is profit share?");
 })();
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export enum AIModels {
   DAVINCI_TURBO = "gpt-3.5-turbo",
   OPEN_AI_EMBEDDING = "text-embedding-ada-002",
   OLLAMA_EMBEDDING = "nomic-embed-text",
+  LLAMA_3 = "llama3",
 }
 
 export enum Databases {
